92d16f2fa1de5dd2af9269f08152412019a9b049
victor matheus <vtinnmatheus@gmail.com>
2025-04-15 11:03:27 -0300

    atividade7

--- arquivo 7.py
+++ arquivo 7.py
@@ -2,7 +2,7 @@
 n=int(input("digite quantos numeros"))
 soma=0
 for x in range(n):
-    valor=float(input("digite um valor"))
+    valor=int(input("digite um valor"))
     soma=soma+valor
 media=soma/n
 
